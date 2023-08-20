ba40723645a527f65b6252565a410268088b6207
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2023-08-20 17:59:44 +0800

    8/20

--- Scripts/goodnotes6.js
+++ Scripts/goodnotes6.js
@@ -8,8 +8,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/goodnotes6.js
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/goodnotes6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/BOBANDJOB/PrivateQX/main/Scripts/goodnotes6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/BOBANDJOB/PrivateQX/main/Scripts/goodnotes6.js
 
 [mitm]
 hostname = isi.csan.goodnotes.com
