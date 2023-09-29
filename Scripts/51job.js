ce31009a9c91d82a32b04a0f166ee654c5f7019c
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2023-09-29 20:55:32 +0800

    中秋快乐

--- Scripts/51job.js
+++ Scripts/51job.js
@@ -1,4 +1,14 @@
-{
+/*
+关键数据
+^https:\/\/appapi\.51job\.com\/api\/market\/get_launch\.php\?productname=51job&version=\d{2}\.\d\.\d&format=json&partner=[a-z0-9]{32}&uuid=[a-z0-9]{32}&guid=[a-z0-9]{32} url script-response-body https://raw.githubusercontent.com/BOBANDJOB/PrivateQX/main/Scripts/51job.js
+hostname：appapi.51job.com
+
+
+
+*/
+var objc = JSON.parse($response.body);
+
+objc = {
   "result" : "1",
   "status" : "1",
   "resultbody" : {
@@ -24,3 +34,4 @@
     ]
   }
 }
+$done({body : JSON.stringify(objc)})
