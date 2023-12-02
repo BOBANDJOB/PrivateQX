7c08cf631a5e6c2f86c6eecbf720100f599e0f11
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2023-12-02 12:49:52 +0800

    keep

--- Scripts/keepScreenAd.js
+++ Scripts/keepScreenAd.js
@@ -9,7 +9,7 @@ hostname：api.gotokeep.com
 */
 var objc = JSON.parse($response.body);
 objc.data.hotFeedSpotAdRequestMoment = 999999;
-objc.data.homePage = 999999;
+objc.data.homePageFeedSpotStep = 999999;
 objc.data.splashAdLoadTimeLimit = 0;
 objc.data.homePageFeedSpotAdRequestMoment = 999999;
 objc.data.pugcLoadPostPreRolls = false;
@@ -19,12 +19,15 @@ objc.data.hotFeedFindSpotAdRequestMoment = 999999;
 objc.data.feedDetailSpotStep = 999999;
 objc.data.followFeedSpotAdRequestMoment = 999999;
 objc.data.feedAdLoadTimeLimit = 0;
+objc.data.homePage_8_FeedSpotStep = 999999;
 objc.data.homePage_8_FeedSpotAdRequestMoment = 999999;
 objc.data.splashWarmAdRequestInterval = 999999;
+objc.data.followFeedSpotStep = 999999;
 objc.data.hotFeedRecommendSpotStep = 999999;
 objc.data.hotFeedRecommendSpotAdRequestMoment = 999999;
 objc.data.hotFeedFindSpotStep = 999999;
 objc.data.feedDetailSpotAdRequestMoment = 999999;
+objc.data.hotFeedSpotStep = 999999;
 objc.data.feedInteractionAdShadowTime = 999999;
 // objc = {
 //   "version" : "1.0.0",
