019b0c1136a151d7010166e9df86b56d7ebe895c
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2024-03-05 14:15:53 +0800

    6

--- Scripts/teraboxvip.js
+++ Scripts/teraboxvip.js
@@ -7,5 +7,5 @@ hostname = www.terabox.com, www.1024tera.com
 
 */
 var objc = JSON.parse($response.body);
-objc.records.vip_type = 3;
+objc.records.vip_type = 1;
 $done({body : JSON.stringify(objc)})
