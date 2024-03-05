e41c727dd2f03708406f436e069e7ea92ec6272c
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2024-03-05 14:45:03 +0800

    vip3

--- Scripts/teraboxvip.js
+++ Scripts/teraboxvip.js
@@ -1,11 +1,11 @@
 /*
 关键数据
-^https:\/\/www\.(1024tera|terabox)\.(com|app)\/api\/user\/getinfo\? url script-response-body https://raw.githubusercontent.com/BOBANDJOB/PrivateQX/main/Scripts/teraboxvip.js
+^https:\/\/www\.terabox\.app\/api\/user\/getinfo\? url script-response-body https://raw.githubusercontent.com/BOBANDJOB/PrivateQX/main/Scripts/teraboxvip.js
 
 hostname = www.terabox.com, www.1024tera.com
 
 
 */
 var objc = JSON.parse($response.body);
-objc.records.vip_type = 1;
+objc.records[0].vip_type = 3;
 $done({body : JSON.stringify(objc)})
