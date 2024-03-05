837d489c7c8373415342f7289499626b531b7dff
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2024-03-05 12:59:52 +0800

    app后缀

--- Scripts/terabox.js
+++ Scripts/terabox.js
@@ -1,6 +1,6 @@
 /*
 关键数据
-^https:\/\/www\.(1024tera|terabox)\.com\/api\/ad\/getconfig\? url script-response-body https://raw.githubusercontent.com/BOBANDJOB/PrivateQX/main/Scripts/terabox.js
+^https:\/\/www\.(1024tera|terabox)\.(com|app)\/api\/ad\/getconfig\? url script-response-body https://raw.githubusercontent.com/BOBANDJOB/PrivateQX/main/Scripts/terabox.js
 
 hostname = www.terabox.com, www.1024tera.com
 
