9218af9526fa4121b9201c9ee673a2b3c3fa7efb
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2024-09-06 23:55:38 +0800

    Delete Scripts/BicaScreenAd.js

--- Scripts/BicaScreenAd.js
+++
@@ -1,8 +0,0 @@
-/*
-^https:\/\/api\.go2778\.com\/announcements\?page=1 url script-response-body https://raw.githubusercontent.com/BOBANDJOB/PrivateQX/main/Scripts/BicaScreenAd.js
-hostname：api.go2778.com
-
-*/
-var objc = JSON.parse($response.body);
-objc.data.announcements.limit = 0;
-$done({body : JSON.stringify(objc)})
