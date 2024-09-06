926b42774ed0480c1a20605370c146ea482e8cb0
BOBANDJOB <36945510+BOBANDJOB@users.noreply.github.com>
2024-09-06 21:21:14 +0800

    更新 BicaScreenAd.js

---
+++ Scripts/BicaScreenAd.js
@@ -0,0 +1,8 @@
+/*
+^https:\/\/api\.go2778\.com\/announcements\?page=1 url script-response-body https://raw.githubusercontent.com/BOBANDJOB/PrivateQX/main/Scripts/BicaScreenAd.js
+hostname：api.go2778.com
+
+*/
+var objc = JSON.parse($response.body);
+objc.data = null
+$done({body : JSON.stringify(objc)})
